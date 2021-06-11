38525a83ab7cb568cd9892f8f9db526314c19dc6
stuffstuffstuf1 <84169470+stuffstuffstuf1@users.noreply.github.com>
2021-06-11 19:11:18 +0800

    6 number lotto simulator
    really repetitive code

---
+++ lotto simulator.py
@@ -0,0 +1,64 @@
+import random
+
+chosen_lotto_one = random.randint(1, 50)
+chosen_lotto_two = random.randint(1, 50)
+chosen_lotto_three = random.randint(1, 50)
+chosen_lotto_four = random.randint(1, 50)
+chosen_lotto_five = random.randint(1, 50)
+chosen_lotto_six = random.randint(1, 50)
+
+your_choice_one = int(input("Enter your first number from the lotto (1 through 49): "))
+your_choice_two = int(input("Enter your first number from the lotto (1 through 49): "))
+your_choice_three = int(input("Enter your first number from the lotto (1 through 49): "))
+your_choice_four = int(input("Enter your first number from the lotto (1 through 49): "))
+your_choice_five = int(input("Enter your first number from the lotto (1 through 49): "))
+your_choice_six = int(input("Enter your first number from the lotto (1 through 49): "))
+
+print("The chosen numbers are: ", chosen_lotto_one, chosen_lotto_two, chosen_lotto_three, chosen_lotto_four, chosen_lotto_five, chosen_lotto_six)
+
+random_thingy = 0
+
+if your_choice_one == chosen_lotto_one:
+    random_thingy += 1 
+else:
+    pass
+
+if your_choice_two == chosen_lotto_two:
+    random_thingy += 1
+else:
+    pass
+
+if your_choice_three == chosen_lotto_three:
+    random_thingy += 1
+else:
+    pass
+
+if your_choice_four == chosen_lotto_four:
+    random_thingy += 1
+else:
+    pass
+
+if your_choice_five == chosen_lotto_five:
+    random_thingy += 1
+else:
+    pass
+
+if your_choice_six == chosen_lotto_six:
+    random_thingy += 1
+else:
+    pass
+
+if random_thingy == 0:
+    print("You got none correct, better luck next time")
+elif random_thingy == 1:
+    print("Good job, you got one correct")
+elif random_thingy == 2:
+    print("Good job, you got TWO correct")
+elif random_thingy == 3:
+    print("Good job, you got THREE correct!")
+elif random_thingy == 4:
+    print("Nice, you got FOUR correct!")
+elif random_thingy == 5:
+    print("Nice, you got FIVE correct!")
+elif random_thingy == 6:
+    print("We have a WINNER!!!!!!")
