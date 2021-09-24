8a432d702398fa7f798a8bdb4981d0945d2d70c1
stuffstuffstuf1 <84169470+stuffstuffstuf1@users.noreply.github.com>
2021-09-24 19:04:11 +0800

    makes it possible to win something
    changes it so that the numbers don't have to be in order

--- lotto simulator.py
+++ lotto simulator.py
@@ -5,18 +5,16 @@ chosen_lotto_two = random.randint(1, 50)
 chosen_lotto_three = random.randint(1, 50)
 chosen_lotto_four = random.randint(1, 50)
 chosen_lotto_five = random.randint(1, 50)
-chosen_lotto_six = random.randint(1, 50)
 bonus_number = random.randint(1, 11)
 
-your_choice_one = int(input("Enter your first number from the lotto (1 through 49): "))
-your_choice_two = int(input("Enter your first number from the lotto (1 through 49): "))
-your_choice_three = int(input("Enter your first number from the lotto (1 through 49): "))
-your_choice_four = int(input("Enter your first number from the lotto (1 through 49): "))
-your_choice_five = int(input("Enter your first number from the lotto (1 through 49): "))
-your_choice_six = int(input("Enter your first number from the lotto (1 through 49): "))
+your_choice_one = int(input("Enter your first number (1 through 49): "))
+your_choice_two = int(input("Enter your second number (1 through 49): "))
+your_choice_three = int(input("Enter your third number (1 through 49): "))
+your_choice_four = int(input("Enter your fourth number (1 through 49): "))
+your_choice_five = int(input("Enter your fifth number (1 through 49): "))
 your_bonus_number = int(input("Enter your bonus number (1 through 10): "))
 
-print("The chosen numbers are: ", chosen_lotto_one, chosen_lotto_two, chosen_lotto_three, chosen_lotto_four, chosen_lotto_five, chosen_lotto_six)
+print("The chosen numbers are: ", chosen_lotto_one, chosen_lotto_two, chosen_lotto_three, chosen_lotto_four, chosen_lotto_five)
 print("The bonus number is: ", bonus_number)
 
 random_thingy = 0
@@ -26,7 +24,57 @@ if your_choice_one == chosen_lotto_one:
 else:
     pass
 
+if your_choice_one == chosen_lotto_two:
+    random_thingy += 1 
+else:
+    pass
+
+if your_choice_one == chosen_lotto_three:
+    random_thingy += 1 
+else:
+    pass
+
+if your_choice_one == chosen_lotto_four:
+    random_thingy += 1 
+else:
+    pass
+
+if your_choice_one == chosen_lotto_five:
+    random_thingy += 1 
+else:
+    pass
+
+if your_choice_two == chosen_lotto_one:
+    random_thingy += 1
+else:
+    pass
+
 if your_choice_two == chosen_lotto_two:
+    random_thingy += 1
+else:
+    pass
+
+if your_choice_two == chosen_lotto_three:
+    random_thingy += 1
+else:
+    pass
+
+if your_choice_two == chosen_lotto_four:
+    random_thingy += 1
+else:
+    pass
+
+if your_choice_two == chosen_lotto_five:
+    random_thingy += 1
+else:
+    pass
+
+if your_choice_three == chosen_lotto_one:
+    random_thingy += 1
+else:
+    pass
+
+if your_choice_three == chosen_lotto_two:
     random_thingy += 1
 else:
     pass
@@ -36,17 +84,62 @@ if your_choice_three == chosen_lotto_three:
 else:
     pass
 
+if your_choice_three == chosen_lotto_four:
+    random_thingy += 1
+else:
+    pass
+
+if your_choice_three == chosen_lotto_five:
+    random_thingy += 1
+else:
+    pass
+
+if your_choice_four == chosen_lotto_one:
+    random_thingy += 1
+else:
+    pass
+
+if your_choice_four == chosen_lotto_two:
+    random_thingy += 1
+else:
+    pass
+
+if your_choice_four == chosen_lotto_three:
+    random_thingy += 1
+else:
+    pass
+
 if your_choice_four == chosen_lotto_four:
     random_thingy += 1
 else:
     pass
 
-if your_choice_five == chosen_lotto_five:
+if your_choice_four == chosen_lotto_five:
     random_thingy += 1
 else:
     pass
 
-if your_choice_six == chosen_lotto_six:
+if your_choice_five == chosen_lotto_one:
+    random_thingy += 1
+else:
+    pass
+
+if your_choice_five == chosen_lotto_two:
+    random_thingy += 1
+else:
+    pass
+
+if your_choice_five == chosen_lotto_three:
+    random_thingy += 1
+else:
+    pass
+
+if your_choice_five == chosen_lotto_four:
+    random_thingy += 1
+else:
+    pass
+
+if your_choice_five == chosen_lotto_five:
     random_thingy += 1
 else:
     pass
@@ -65,13 +158,11 @@ elif random_thingy == 2:
 elif random_thingy == 3:
     print("Good job, you got THREE correct!")
 elif random_thingy == 4:
-    print("Nice, you got FOUR correct!")
+    print("Ayo, you got FOUR correct!")
 elif random_thingy == 5:
-    print("Nice, you got FIVE correct!")
-elif random_thingy == 6:
     print("We have a WINNER!!!!!!")
 
 if bonus_number_thingy == 1:
     print("You got the bonus number correct!")
 else:
-    print("You didn't get the bonus number right...")
+    print("You didn't get the bonus number right.")
